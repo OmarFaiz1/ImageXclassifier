1d91d37fe114c0df0613b39de16ce86c2b567f3c
Omar Faiz <omarsap6@gmail.com>
2025-04-18 14:07:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -193,5 +193,7 @@ def automate_chatbot(email, image_name):
         driver.quit()
 
 # --- Run Server ---
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
